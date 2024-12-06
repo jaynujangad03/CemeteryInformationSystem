22aa6a063187f116025abe1e906c82a841c69659
jaynujangad03 <jaynujangad03@gmail.com>
2024-12-06 21:24:06 +0800

    dec 6

--- cemeterymapping/about.php
+++ cemeterymapping/about.php
@@ -1,15 +1,27 @@
-
 <style type="text/css">
 
+    .scrollable-content {
+        height: 300px; /* Set the height to control the scrollable area */
+        overflow-y: scroll;
+        padding-right: 10px; /* Optional: to prevent scrollbar from overlapping the content */
+    }
+
+    .scrollable-content img {
+        width: 100px;
+        height: 100px;
+        margin-right: 10px;
+    }
 </style>
+
 <div class="row"> 
     <div class="col-lg-12">
-        <div><img src="img/rhonamae.jpg" title="img" style="width: 100px;height: 100px"> Rhona Mae Llanto - Project Manager </div>
-        <div><img src="img/jaynu.jpg" title="img" style="width: 100px;height: 100px"> Jaynu D. Jangad - Programmer/backend</div>
-        <div><img src="img/mikel.jpg" title="img" style="width: 100px;height: 100px"> Michael Dela Cuesta - Web Developer/frontend</div>
-        <div><img src="img/japet.jpg" title="img" style="width: 100px;height: 100px"> Jafaith Otlang - System Analyst</div>
-        <div><img src="img/wayne.jpg" title="img" style="width: 100px;height: 100px"> Wayne Anave - Researcher</div> 
-        <div><img src="img/ (1).jpg" title="img" style="width: 100px;height: 100px"> Juswa - Documenter</div> 
+        <div class="scrollable-content">
+            <div><img src="img/rhonamae.jpg" title="img"> Rhona Mae Llanto - Project Manager </div>
+            <div><img src="img/jaynu.jpg" title="img"> Jaynu D. Jangad - Programmer/backend</div>
+            <div><img src="img/mikel.jpg" title="img"> Michael Dela Cuesta - Web Developer/frontend</div>
+            <div><img src="img/japet.jpg" title="img"> Jafaith Otlang - System Analyst</div>
+            <div><img src="img/wayne.jpg" title="img"> Wayne Anave - Researcher</div> 
+            <div><img src="img/ (1).jpg" title="img"> Juswa - Documenter</div> 
+        </div>
     </div>
-        
 </div>
